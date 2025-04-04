49339435909fa42928e3450ac8be8f8cf26fdae0
Luka Joze Puslar <ljpuslar@login34.chn.perlmutter.nersc.gov>
2025-04-04 11:14:30 -0700

    updates

--- src/RSA_nD_metric_plot.py
+++ src/RSA_nD_metric_plot.py
@@ -3,19 +3,21 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-nevents = 10000
+nevents = 100
+grid_res = 10
+
 Neffs_file_name = f'Neffs_3d1_N{nevents}'
 Sigma_file_name = f'Sigmas_3d1_N{nevents}'
 A_file_name = f'Ad_3d1_N{nevents}'
 B_file_name = f'B_3d1_N{nevents}'
 C_file_name = f'Au_3d1_N{nevents}'
 
+Neffs_file_name = f'Neffs_3d1_N{nevents}_grid{grid_res}'
+Sigma_file_name = f'Sigmas_3d1_N{nevents}_grid{grid_res}'
+A_file_name = f'Ad_3d1_N{nevents}_grid{grid_res}'
+B_file_name = f'B_3d1_N{nevents}_grid{grid_res}'
+C_file_name = f'Au_3d1_N{nevents}_grid{grid_res}'
 
-# np.save('/pscratch/sd/l/ljpuslar/RSA/RSA/src/temp_results/Metrics/' + Neffs_file_name, Neffs)
-# np.save('/pscratch/sd/l/ljpuslar/RSA/RSA/src/temp_results/Metrics/' + Sigma_file_name, Sigmas)
-# np.save('/pscratch/sd/l/ljpuslar/RSA/RSA/src/temp_results/Metrics/' + A_file_name, Ad)
-# np.save('/pscratch/sd/l/ljpuslar/RSA/RSA/src/temp_results/Metrics/' + B_file_name, B)
-# np.save('/pscratch/sd/l/ljpuslar/RSA/RSA/src/temp_results/Metrics/' + C_file_name, Au)
 
 # Loading the saved files into variables with '_loaded' suffix
 Neffs_loaded = np.load('temp_results/Metrics/' + Neffs_file_name + '.npy')
@@ -24,7 +26,7 @@ Ad_loaded = np.load('temp_results/Metrics/' + A_file_name + '.npy')
 B_loaded = np.load('temp_results/Metrics/' + B_file_name + '.npy')
 Au_loaded = np.load('temp_results/Metrics/' + C_file_name + '.npy')
 
-
+# N_EFF PLOT:
 # Flatten the grids and function values for plotting
 x_vals = Ad_loaded.flatten()
 y_vals = Au_loaded.flatten()
@@ -36,7 +38,7 @@ fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111, projection='3d')
 
 # Scatter plot with a color map
-scat = ax.scatter(x_vals, y_vals, z_vals, c=N_eff, cmap='viridis', s=60, alpha=0.8)
+scat = ax.scatter(x_vals, y_vals, z_vals, c=N_eff, cmap='bwr', s=60, alpha=0.8)
 ax.scatter(0.68,0.68,0.98, s=100, c='red')
 
 # Labels and title with larger font size and bold styling
@@ -62,8 +64,7 @@ ax.grid(True, linestyle='--', color='gray', alpha=0.6)
 
 plt.show()
 
-
-# Assuming the variables Ad_loaded, Au_loaded, B_loaded, and Neffs_loaded are already defined and loaded with data
+# MU PLOT
 # Flatten the grids and function values for plotting
 x_vals = Ad_loaded.flatten()
 y_vals = Au_loaded.flatten()
@@ -75,18 +76,21 @@ fig = plt.figure(figsize=(10, 7))
 ax = fig.add_subplot(111, projection='3d')
 
 # Scatter plot with a color map
-scat = ax.scatter(x_vals, y_vals, z_vals, c=Sigmas, cmap='viridis', s=60, alpha=0.8)
+norm = TwoSlopeNorm(vmin=np.min(Sigmas), vcenter=0, vmax=np.max(Sigmas))
+scat = ax.scatter(x_vals, y_vals, z_vals, c=Sigmas, cmap='bwr',norm=norm, s=60, alpha=0.8)
 ax.scatter(0.68,0.68,0.98, s=100, c='red')
 
 # Labels and title with larger font size and bold styling
 ax.set_xlabel('a_d', fontsize=14, fontweight='bold')
 ax.set_ylabel('a_u', fontsize=14, fontweight='bold')
 ax.set_zlabel('b', fontsize=14, fontweight='bold')
-ax.set_title('Sigma', fontsize=16, fontweight='bold')
+sig_label = r"$\ln \left( \left| \frac{1 - \mu}{\sigma_{1 - \mu}} \right| \right)$"
+
+ax.set_title(sig_label, fontsize=16, fontweight='bold')
 
 # Add a colorbar with improved style
 colorbar = fig.colorbar(scat)
-colorbar.set_label('Sigma', fontsize=12, fontweight='bold')
+colorbar.set_label(sig_label, fontsize=12, fontweight='bold')
 colorbar.ax.tick_params(labelsize=10)
 
 # Improve layout and tight layout
