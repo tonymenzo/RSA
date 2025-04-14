def3cd533e867c9f1f836f62bca38f828cfbd09f
Luka Joze Puslar <ljpuslar@login19.chn.perlmutter.nersc.gov>
2025-04-14 08:31:18 -0700

    added pT data

--- data/RSA_nD_data_converter_run.py
+++ data/RSA_nD_data_converter_run.py
@@ -8,8 +8,8 @@ import subprocess
 # Define the parameters
 aLund = 0.68 # Example value for aLund
 bLund = 0.98  # Example value for bLund
-sigma = 0.300  # Example value for sigma
-nEvent = 100000  # Example value for nEvent
+sigma = 0.335  # Example value for sigma
+nEvent = 100  # Example value for nEvent
 
 # Define parameters
 aExtraDQuark = 0
@@ -70,6 +70,7 @@ acceptReject_PATH = f"{filename_base}_accept_reject_z.txt"
 mT2_PATH = f"{filename_base}_mT2.txt"
 fPrel_PATH = f"{filename_base}_fPrel.txt"
 id_PATH = f"{filename_base}_pid.txt"
+pT_PATH = f"{filename_base}_pT.txt"
 
 # Print the paths to check
 print("Hadron Path: ", hadron_PATH)
@@ -77,6 +78,7 @@ print("AcceptReject Path: ", acceptReject_PATH)
 print("mT2 Path: ", mT2_PATH)
 print("fPrel Path: ", fPrel_PATH)
 print("id Path: ", id_PATH)
+print("pT Path: ", pT_PATH)
 
 # Use formatted string literals (f-strings) to automatically generate the paths
 # filename_base = f"/global/homes/l/ljpuslar/RSA/RSA/data/structured_data/pgun_uubar_a_{aLund}_b_{bLund}_sigma_{sigma}_N_{nEvent:.1e}"
@@ -116,6 +118,7 @@ args = [
     "--data_path_mT2", mT2_PATH,
     "--data_path_hadrons", hadron_PATH,
     "--data_path_id", id_PATH,
+    "--data_path_pT", pT_PATH,
     "--write_path_id_mT2_accept_reject", write_id_mT2_acceptReject_PATH,
     "--write_path_fPrel", write_fPrel_PATH,
     "--write_path_hadrons", write_hadron_PATH,
